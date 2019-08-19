c6fb12b442ed28af8a962449f54e6cd992c97618
yar0316_note <yaworkon@gmail.com>
2019-08-19 16:16:25 +0900

    Change to delete if the end of the string is ","

--- JS/conversion.js
+++ JS/conversion.js
@@ -3,12 +3,18 @@
  * @param {String} input 入力された文字列
  */
 function conversion(input) {
-  console.log(input.match(/\s\n/));
-  console.log(input.match(/\n/));
+
   //半角スペースがある場合とない場合に対応
+  var converted = "";
   if (input.match(/\s\n/g) != null) {
-    return input.replace(/\s\n/g, ",").slice(0, -1);
-  } else if (input.match(/\n/g) != null) {
-    return input.replace(/\n/g, ",").slice(0, -1);
+    converted = input.replace(/\s\n/g, ",");
+  } 
+  if (input.match(/\n/g) != null) {
+    converted = input.replace(/\n/g, ",");
   }
+
+  if (converted.slice(-1) === ",") {
+    converted = converted.slice(0, -1);
+  }
+  return converted;
 }
