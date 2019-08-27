c60aa5242fd2dc8d345916daa762e565711d741b
yar0316_note <yaworkon@gmail.com>
2019-08-27 20:17:14 +0900

    細かな修正

--- JS/conversion.js
+++ JS/conversion.js
@@ -4,14 +4,13 @@
  * @returns {string} カンマ区切りに変換された文字列
  */
 function conversion(input) {
-
   //半角スペースがある場合とない場合に対応
   var converted = "";
-  if (input.match(/\s\n/g) != null) {
-    converted = input.replace(/\s\n/g, ",");
-  } 
   if (input.match(/\n/g) != null) {
     converted = input.replace(/\n/g, ",");
+  }
+  if (input.match(/\s\n/g) != null) {
+    converted = input.replace(/\s\n/g, ",");
   }
 
   if (converted.slice(-1) === ",") {
