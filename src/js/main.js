9bee44200bea0929d4f2131eecf772b39f384a34
yar0316_note <yaworkon@gmail.com>
2019-11-18 15:29:55 +0900

    add electron app ver.

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let appWindow
+
+function createWindow () {
+  // Create the browser window.
+  appWindow = new BrowserWindow({
+    width: 400,
+    height: 550,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index of the app.
+  appWindow.loadFile('index.html')
+
+  appWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  appWindow.on('closed', () => {
+    appWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (appWindow === null) {
+    createWindow()
+  }
+})
